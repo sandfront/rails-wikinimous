412b987f26d2ab45131af559908235580850ffe2
James Aung <therealaung@gmail.com>
2017-08-15 16:23:38 +0100

    half implement sessions and users for auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-
+  before_action :authorize
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,3 @@
 class Article < ApplicationRecord
+  serialize :content
 end
